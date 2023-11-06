4ba62f9535f99cc9d73c75373c5c76603e3e89f4
Sahil Gidwani <79597840+sahil-gidwani@users.noreply.github.com>
2023-11-06 13:42:24 +0530

    Update bank.sol

--- bank.sol
+++ bank.sol
@@ -1,40 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
-contract DBank
-{
-    address Owner;
+contract DBank {
+    address Owner;  // Store the address of the owner of the contract
 
-    // We are creating the Mapping for the Adding and Withdrawal of Amount in Account
+    // Create a mapping to track the account balances of users
     mapping(address => uint) Balance;
 
-    // Constructor for the address of the Owner
-    constructor()
-    {
+    // Constructor that sets the owner of the contract to the address that deploys it
+    constructor() {
         Owner = msg.sender;
     }
 
-    // Function for adding Ethereum to the Account
-    function addBalance(uint amount) public returns (uint)
-    {
-        // First, we have to check if it is the Owner's Account or Not
-        require(msg.sender == Owner, "Yes, it is Owner's Account !!");
+    // Function for the owner to add Ethereum to the account
+    function addBalance(uint amount) public returns (uint) {
+        // Ensure that only the owner can add funds to the contract
+        require(msg.sender == Owner, "Only the owner can add funds.");
         Balance[msg.sender] = Balance[msg.sender] + amount;
         return Balance[msg.sender];
     }
 
-    // Function to Get the Balance from an Account
-    function getBalance() public view returns (uint)
-    {
+    // Function to get the balance of the sender's account
+    function getBalance() public view returns (uint) {
         return Balance[msg.sender];
     }
 
-    // Function for the owner to withdraw a specified amount
-    function withdraw(uint amount) public returns (uint)
-    {
-        require(msg.sender == Owner, "Only the owner can withdraw");
-        require(Balance[msg.sender] >= amount, "Insufficient balance for withdrawal");
-        
+    // Function for the owner to withdraw a specified amount from the contract
+    function withdraw(uint amount) public returns (uint) {
+        // Ensure that only the owner can withdraw funds
+        require(msg.sender == Owner, "Only the owner can withdraw.");
+        // Check if the owner has sufficient balance for withdrawal
+        require(Balance[msg.sender] >= amount, "Insufficient balance for withdrawal.");
         Balance[msg.sender] = Balance[msg.sender] - amount;
         return Balance[msg.sender];
     }
